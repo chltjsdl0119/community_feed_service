6b7f88813a9c187af3f2bb9710f3fd0ee413dd53
seungwon-choi <bestwondev@gmail.com>
2024-10-03 14:14:11 +0900

    initial commit

---
+++ src/test/java/com/seungwon/community_feed_service/CommunityFeedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungwon.community_feed_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityFeedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
