9e3d3d635c1b0b1012654d9929556b8a64a99037
Seungwon-Choi <bestwondev@gmail.com>
2025-01-03 15:46:34 +0900

    initial commit

---
+++ src/main/java/seungwon/community_feed_service/CommunityFeedServiceApplication.java
@@ -0,0 +1,13 @@
+package seungwon.community_feed_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityFeedServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityFeedServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/seungwon/community_feed_service/CommunityFeedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package seungwon.community_feed_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityFeedServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
