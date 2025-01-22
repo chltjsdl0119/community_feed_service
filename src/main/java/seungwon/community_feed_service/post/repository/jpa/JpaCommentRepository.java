542937e709c5b8e80cd5b9a18a4e2368dd0608e2
Seungwon-Choi <bestwondev@gmail.com>
2025-01-23 00:25:29 +0900

    feat: Comment 수정 기능 JPQL

---
+++ src/main/java/seungwon/community_feed_service/post/repository/jpa/JpaCommentRepository.java
@@ -0,0 +1,16 @@
+package seungwon.community_feed_service.post.repository.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import seungwon.community_feed_service.post.repository.entity.comment.CommentEntity;
+
+public interface JpaCommentRepository extends JpaRepository<CommentEntity, Long> {
+
+    @Modifying
+    @Query(value = "UPDATE CommentEntity c " +
+            "SET c.content = :#{#commentEntity.getContent()}," +
+            "c.updDt = now()" +
+            "WHERE c.id = :#{#commentEntity.getId()} ")
+    void updateCommentEntity(CommentEntity commentEntity);
+}
